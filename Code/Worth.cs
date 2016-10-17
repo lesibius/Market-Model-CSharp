98a90910e6c360300b4b4f5ff2456c98b007be86
lesibius <clementin.castellano@gmail.com>
2016-10-17 23:28:52 +0200

    Update Worth.cs

--- Code/Worth.cs
+++ Code/Worth.cs
@@ -36,27 +36,48 @@ namespace Market_Model_CSharp
         /// </summary>
         public decimal Worth { get; set; }
 
-        /// <summary>
-        /// Convert a <c>ValueClass</c> instance to a new <c>ValueClass</c> instance in the specified currency, using a <c>CurrencyExchange</c> instance
-        /// </summary>
-        /// <param name="forex"><c>CurrencyExchange</c> instance which provide the exchange rate</param>
-        /// <param name="val"><c>ValueClass</c> instance to convert</param>
-        /// <returns>The original <c>ValueClass</c> instance converted in the new currency</returns>
-        public static ValueClass Convert(CurrencyExchange forex, ValueClass val)
-        {
-            //TODO: call the forex.Convert method to return a new val
-            //Use a delegate???
-            return (val);
-        }
 
-        /*
+        public delegate ValueClass DefaultValueConversion(ValueClass ValueToConvert);
+
+        /// <summary>
+        /// Override the <c>+</c> operator for <c>ValueClass</c> and <c>decimal</c>
+        /// This assume that the decimal is expressed in term of currency of the <c>ValueClass</c> instance
+        /// </summary>
+        /// <param name="v1">Value in the + operation</param>
+        /// <param name="val">Value (expressed in term of currency of v1) to add</param>
+        /// <returns></returns>
+        public static ValueClass operator +(ValueClass v1, decimal val)
+        {
+            return new ValueClass(v1.Currency, v1.Worth + val);
+        }
+        /// <summary>
+        /// Override the <c>+</c> operator for <c>ValueClass</c> and <c>decimal</c>
+        /// This assume that the decimal is expressed in term of currency of the <c>ValueClass</c> instance
+        /// </summary>
+        /// <param name="val">Value (expressed in term of currency of v1) to add</param>
+        /// <param name="v1">Value in the + operation</param>
+        /// <returns></returns>
+        public static ValueClass operator +(decimal val, ValueClass v1) { return v1 + val; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
         public static ValueClass operator +(ValueClass v1, ValueClass v2)
         {
-            //Convert v2 currency
-
-            //Add v1 and v2 Worth
+            //If both currencies are the same, just add worth, else throw an exception
+            if(v1.Currency.Equals(v2.Currency)){ return new ValueClass(v1.Currency, v1.Worth + v2.Worth);}
+            else{throw new System.ArgumentException("Cannot add two ValueClass with different currencies","v1 and v2");}
         }
-        */
+
+
+        public override string ToString()
+        {
+            return(this.Currency.CodeISO.ToString()+ " " + this.Worth.ToString());
+        }
 
     }
+    
 }
